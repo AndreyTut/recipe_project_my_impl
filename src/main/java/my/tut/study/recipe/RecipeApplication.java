79452a40ed8dd45f7f0f7098f84e8675950086b2
Andrey <andreww@ukr.net>
2020-08-12 15:47:09 +0300

    Initial commit

---
+++ src/main/java/my/tut/study/recipe/RecipeApplication.java
@@ -0,0 +1,15 @@
+package my.tut.study.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+
+        System.out.println("hi there _________________$$$$$$$$%%%%%%%%%%%%%%%%%%################");
+    }
+
+}
---
+++ src/test/java/my/tut/study/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package my.tut.study.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
